1c052cedba6c5d195b17e43e0cdc4f91fa55022d
Mingqian94 <yuchen1994wang@163.com>
2026-05-12 22:35:16 +0800

    fix: add basename for router

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import RuleCardPage from "@/pages/RuleCardPage";
 export default function App() {
   return (
     <LanguageProvider>
-      <Router>
+      <Router basename="/board-game-rule-doctor">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/game/:id" element={<RuleCardPage />} />
